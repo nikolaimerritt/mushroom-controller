d9c7385cc3cfa78c27e1bf723bb110b630d668a9
Nikolai Merritt <nikolaimerritt20@gmail.com>
2024-03-26 21:41:15 +0000

    Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -6,73 +6,73 @@ SHT31 temp_humidity_sensor;
 bool is_heater_on = true;
 
 void clearLine(uint8_t lineNumber) {
-  for (uint8_t c = 0; c < LINE_LENGTH; c++) {
-    lcd.setCursor(c, lineNumber);
-    lcd.write(' ');
-  }
-  lcd.setCursor(0, lineNumber);
+    for (uint8_t c = 0; c < LINE_LENGTH; c++) {
+        lcd.setCursor(c, lineNumber);
+        lcd.write(' ');
+    }
+    lcd.setCursor(0, lineNumber);
 }
 
 void PressButton(uint8_t pin) {
-  digitalWrite(pin, LOW);
-  delay(100);
-  digitalWrite(pin, HIGH);
-  delay(1000);
+    digitalWrite(pin, LOW);
+    delay(100);
+    digitalWrite(pin, HIGH);
+    delay(1000);
 }
 
 void setup() {
-  lcd.init();
-  lcd.backlight();
+    lcd.init();
+    lcd.backlight();
 
-  Wire.begin();
-  Wire.setClock(WIRE_CLOCK);
+    Wire.begin();
+    Wire.setClock(WIRE_CLOCK);
 
-  temp_humidity_sensor.begin();
+    temp_humidity_sensor.begin();
 
-  co2_sensor.begin();
+    co2_sensor.begin();
 
-  clearLine(0);
-  lcd.print("Fartuino 1660 Ti v3");
+    clearLine(0);
+    lcd.print("Fartuino 1660 Ti v3");
 
-  digitalWrite(ON_PIN, HIGH);
-  digitalWrite(OFF_PIN, HIGH);
+    digitalWrite(ON_PIN, HIGH);
+    digitalWrite(OFF_PIN, HIGH);
 
-  pinMode(ON_PIN, OUTPUT);
-  pinMode(OFF_PIN, OUTPUT);
+    pinMode(ON_PIN, OUTPUT);
+    pinMode(OFF_PIN, OUTPUT);
 
-  PressButton(ON_PIN);
+    PressButton(ON_PIN);
 }
 
 void loop() {
-  temp_humidity_sensor.read(false);
-  float temperature = temp_humidity_sensor.getTemperature();
-  float humidity = temp_humidity_sensor.getHumidity();
+    temp_humidity_sensor.read(false);
+    float temperature = temp_humidity_sensor.getTemperature();
+    float humidity = temp_humidity_sensor.getHumidity();
 
-  clearLine(1);
-  lcd.print("Humidity:  ");
-  lcd.print(humidity);
-  lcd.write('%');
+    clearLine(1);
+    lcd.print("Humidity:  ");
+    lcd.print(humidity);
+    lcd.write('%');
 
-  clearLine(2);
-  lcd.print("Temp:      ");
-  lcd.print(temperature);
-  lcd.print(" C");
+    clearLine(2);
+    lcd.print("Temp:      ");
+    lcd.print(temperature);
+    lcd.print(" C");
 
-  if (co2_sensor.readMeasurement()) {
-    uint16_t const co2 = co2_sensor.getCO2();
-    clearLine(3);
-    lcd.print("CO2:       ");
-    lcd.print(co2);
-    lcd.print(" PPM");
-  }
+    if (co2_sensor.readMeasurement()) {
+        uint16_t const co2 = co2_sensor.getCO2();
+        clearLine(3);
+        lcd.print("CO2:       ");
+        lcd.print(co2);
+        lcd.print(" PPM");
+    }
 
-  if (temperature > MAX_TEMP && is_heater_on) {
-    PressButton(OFF_PIN);
-    is_heater_on = false;
-  } else if (temperature < MIN_TEMP && !is_heater_on) {
-    PressButton(ON_PIN);
-    is_heater_on = true;
-  }
+    if (temperature > MAX_TEMP && is_heater_on) {
+        PressButton(OFF_PIN);
+        is_heater_on = false;
+    } else if (temperature < MIN_TEMP && !is_heater_on) {
+        PressButton(ON_PIN);
+        is_heater_on = true;
+    }
 
-  delay(5000);
+    delay(5000);
 }
